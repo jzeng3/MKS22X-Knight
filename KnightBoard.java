d49233a7eebaf97d9fb7456d542c6c270e29be6f
Jamie Zeng <jzeng3@stuy.edu>
2019-02-12 10:16:59 -0500

    coded and tested toString

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,4 +1,9 @@
 public class KnightBoard{
+
+  public static void main(String[] args){
+    KnightBoard test = new KnightBoard(3,3);
+    System.out.println(test);
+  }
   private int[][] boardSequence;
 /*  @throws IllegalArgumentException when either parameter is negative.
 */
@@ -7,10 +12,31 @@ public class KnightBoard{
       throw new IllegalArgumentException();
     }
     boardSequence = new int[startingRows][startingCols];
+    // initialize with all 0s
+    for (int r = 0; r < startingRows; r++){
+      for (int c = 0; c < startingCols; c++){
+        boardSequence[r][c] = 0;
+      }
+    }
   }
 
   public String toString(){
-    return "";
+    String boardStr = "";
+    for (int r = 0; r < boardSequence.length; r++){
+      for (int c = 0; c < boardSequence[0].length; c++){
+        // add extra space if number is less than 10 for nice formatting
+        if (boardSequence[r][c] < 10){
+          boardStr += " " + boardSequence[r][c];
+        }
+        else{
+          boardStr += boardSequence[r][c];
+        }
+        if (c == boardSequence[0].length - 1){
+          boardStr += "\n";
+        }
+      }
+    }
+    return boardStr;
   }
 /*  @throws IllegalStateException when the board contains non-zero values.
   @throws IllegalArgumentException when either parameter is negative
