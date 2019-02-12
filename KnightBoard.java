dc0e4b5ac4270420d00968d30ce5625c17b9c80c
Jamie Zeng <jzeng3@stuy.edu>
2019-02-12 10:10:17 -0500

    Added KnightBoard and constructor, public method headers

---
+++ KnightBoard.java
@@ -0,0 +1,21 @@
+public class KnightBoard{
+/*  @throws IllegalArgumentException when either parameter is negative.
+*/
+  public KnightBoard(int startingRows,int startingCols){
+
+  }
+
+  public String toString(){
+    return "";
+  }
+/*  @throws IllegalStateException when the board contains non-zero values.
+  @throws IllegalArgumentException when either parameter is negative
+  or out of bounds.*/
+  public int countSolutions(int startingRow, int startingCol){
+    return 0;
+  }
+  // level is the # of the knight
+  private boolean solveH(int row ,int col, int level){
+    return false;
+  }
+}
